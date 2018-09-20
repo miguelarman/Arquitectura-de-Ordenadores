71b54a5cf6794db6f21de011f272dfa926b2736a
miguelarman <miguelarman@gmail.com>
2018-09-20 13:41:17 +0200

    Initial commit

---
+++ Pr_1/rtl/processor.vhd
@@ -0,0 +1,32 @@
+--------------------------------------------------------------------------------
+-- Procesador MIPS con pipeline curso Arquitectura 2018-19
+--
+-- (INCLUIR AQUI LA INFORMACION SOBRE LOS AUTORES)
+--
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity processor is
+   port(
+      Clk         : in  std_logic; -- Reloj activo flanco subida
+      Reset       : in  std_logic; -- Reset asincrono activo nivel alto
+      -- Instruction memory
+      IAddr      : out std_logic_vector(31 downto 0); -- Direccion
+      IDataIn    : in  std_logic_vector(31 downto 0); -- Dato leido
+      -- Data memory
+      DAddr      : out std_logic_vector(31 downto 0); -- Direccion
+      DRdEn      : out std_logic;                     -- Habilitacion lectura
+      DWrEn      : out std_logic;                     -- Habilitacion escritura
+      DDataOut   : out std_logic_vector(31 downto 0); -- Dato escrito
+      DDataIn    : in  std_logic_vector(31 downto 0)  -- Dato leido
+   );
+end processor;
+
+architecture rtl of processor is 
+
+begin   
+ 
+end architecture;
