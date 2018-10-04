86f86cb1598e32834d422d1f6e7aa691d404d4c7
albertogk <agk1esoc@gmail.com>
2018-10-04 23:34:35 +0200

    Bono, bonito, barato

--- Pr_1/Uniciclo/rtl/alu_control.vhd
+++ Pr_1/Uniciclo/rtl/alu_control.vhd
@@ -1,7 +1,8 @@
 --------------------------------------------------------------------------------
 -- Bloque de control para la ALU. Arq0 2018.
 --
--- (INCLUIR AQUI LA INFORMACION SOBRE LOS AUTORES, Quitar este mensaje)
+-- Miguel Arconada Manteca        miguel.arconada@estudiante.uam.es
+-- Alberto González Klein         alberto.gonzalezk@estudiante.uam.es
 --
 --------------------------------------------------------------------------------
 
@@ -12,8 +13,8 @@ use ieee.std_logic_unsigned.all;
 entity alu_control is
    port (
       -- Entradas:
-      ALUOp  : in std_logic_vector (2 downto 0); -- Codigo control desde la unidad de control
-      Funct  : in std_logic_vector (5 downto 0); -- Campo "funct" de la instruccion
+      ALUOp      : in std_logic_vector (2 downto 0); -- Codigo control desde la unidad de control
+      Funct      : in std_logic_vector (5 downto 0); -- Campo "funct" de la instruccion
       -- Salida de control para la ALU:
       ALUControl : out std_logic_vector (3 downto 0) -- Define operacion a ejecutar por ALU
    );
@@ -21,31 +22,35 @@ end alu_control;
 
 architecture rtl of alu_control is
 
+   subtype t_aluOp   is std_logic_vector (2 downto 0);
+   subtype t_control is std_logic_vector (3 downto 0);
+   subtype t_funct   is std_logic_vector (5 downto 0);
+
    -- Codigos para ALUOp
-   constant RTYPE : std_logic_vector (2 downto 0) := "000";
-   constant BEQ : std_logic_vector (2 downto 0) := "001";
-   constant ADDI : std_logic_vector (2 downto 0) := "010";
-   constant MEM : std_logic_vector (2 downto 0) := "011";
-   constant LUI : std_logic_vector (2 downto 0) := "100";
+   constant RTYPE    : t_aluOp := "000";
+   constant BEQ      : t_aluOp := "001";
+   constant ADDI     : t_aluOp := "010";
+   constant MEM      : t_aluOp := "011";
+   constant LUI      : t_aluOp := "100";
 
 
    -- Codigos de control
-   constant ALU_OR   : std_logic_vector (3 downto 0) := "0111";   
-   constant ALU_NOT  : std_logic_vector (3 downto 0) := "0101";
-   constant ALU_XOR  : std_logic_vector (3 downto 0) := "0110";
-   constant ALU_AND  : std_logic_vector (3 downto 0) := "0100";
-   constant ALU_SUB  : std_logic_vector (3 downto 0) := "0001";
-   constant ALU_ADD  : std_logic_vector (3 downto 0) := "0000";
-   constant ALU_SLT  : std_logic_vector (3 downto 0) := "1010";
-   constant ALU_S16  : std_logic_vector (3 downto 0) := "1101";
+   constant ALU_OR   : t_control := "0111";   
+   constant ALU_NOT  : t_control := "0101";
+   constant ALU_XOR  : t_control := "0110";
+   constant ALU_AND  : t_control := "0100";
+   constant ALU_SUB  : t_control := "0001";
+   constant ALU_ADD  : t_control := "0000";
+   constant ALU_SLT  : t_control := "1010";
+   constant ALU_S16  : t_control := "1101";
 
 
    -- Codifcacion de funct
-   constant F_ADD : std_logic_vector(5 downto 0) := "100000";
-   constant F_AND : std_logic_vector(5 downto 0) := "100100";
-   constant F_OR : std_logic_vector(5 downto 0) := "100101";
-   constant F_SUB : std_logic_vector(5 downto 0) := "100010";
-   constant F_XOR : std_logic_vector(5 downto 0) := "100110";
+   constant F_ADD    : t_funct := "100000";
+   constant F_AND    : t_funct := "100100";
+   constant F_OR     : t_funct := "100101";
+   constant F_SUB    : t_funct := "100010";
+   constant F_XOR    : t_funct := "100110";
 
 begin
 
@@ -54,17 +59,17 @@ begin
       case ALUOp is
          when RTYPE  =>
             case Funct is
-               when F_ADD => ALUControl <= ALU_ADD;
-               when F_AND => ALUControl <= ALU_AND;
-               when F_OR => ALUControl <= ALU_OR;
-               when F_SUB => ALUControl <= ALU_SUB;
-               when F_XOR => ALUControl <= ALU_XOR;
+               when F_ADD  => ALUControl <= ALU_ADD;
+               when F_AND  => ALUControl <= ALU_AND;
+               when F_OR   => ALUControl <= ALU_OR;
+               when F_SUB  => ALUControl <= ALU_SUB;
+               when F_XOR  => ALUControl <= ALU_XOR;
                when others => ALUControl <= "----";
             end case;
-         when BEQ  => ALUControl <= ALU_SUB;
-         when ADDI  => ALUControl <= ALU_ADD;
-         when MEM  => ALUControl <= ALU_ADD;
-         when LUI  => ALUControl <= ALU_S16;
+         when BEQ    => ALUControl <= ALU_SUB;
+         when ADDI   => ALUControl <= ALU_ADD;
+         when MEM    => ALUControl <= ALU_ADD;
+         when LUI    => ALUControl <= ALU_S16;
          when others => ALUControl <= "----";
       end case;
    end process;
